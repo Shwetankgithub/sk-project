9759e73e7e0bde21a926343c3ff5984fa22c267d
Shwetank <shwetanksk@gmail.com>
2020-07-11 00:11:03 +0530

    test

--- helidon-quickstart-mp/src/main/java/io/helidon/examples/quickstart/mp/GreetingProvider.java
+++ helidon-quickstart-mp/src/main/java/io/helidon/examples/quickstart/mp/GreetingProvider.java
@@ -23,7 +23,7 @@ public class GreetingProvider {
     @Inject
     public GreetingProvider(@ConfigProperty(name = "app.greeting") String message) {
         this.message.set(messagessssssssssss);
-        System.out.println("hello from master22");
+       System.out.println("hello from branch");
     }
 
     String getMessage() {
